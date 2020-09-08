4f637e9f255aae8cb11c6c61039060b7b4786dc1
Maran <maran.happysanz@gmail.com>
2020-09-08 14:16:29 +0530

    config update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/spveluapp/';
+//$config['base_url'] = 'http://localhost/spveluapp/';
+$config['base_url'] = 'http://happysanz.in/spveluapp/';
 
 /*
 |--------------------------------------------------------------------------
