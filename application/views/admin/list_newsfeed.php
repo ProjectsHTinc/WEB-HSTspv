2a5e3c725506c850015c24f2a7a1a126e1ec97d2
Maran <maran.happysanz@gmail.com>
2020-09-24 16:22:31 +0530

    Update list_newsfeed.php

--- application/views/admin/list_newsfeed.php
+++ application/views/admin/list_newsfeed.php
@@ -60,6 +60,7 @@
 												//if the string doesn't contain any space then it will cut without word basis.
 												$string = $endPoint? substr($stringCut, 0, $endPoint) : substr($stringCut, 0);
 												$string .= '...';
+											}
 										?>
                                         <tr>
                                             <td><?php echo $i; ?></td>
