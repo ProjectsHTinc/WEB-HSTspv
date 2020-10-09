2974994b56e6de6273172e5f2700605a2b2145aa
Maran <maran.happysanz@gmail.com>
2020-10-09 15:13:19 +0530

    API update

--- application/controllers/Apiuser.php
+++ application/controllers/Apiuser.php
@@ -12,7 +12,6 @@ class Apiuser extends CI_Controller {
         parent::__construct();
 		$this->load->model("apiusermodel");
 		$this->load->helper("url");
-		$this->load->library('session');
     }
 
 	public function checkMethod()
